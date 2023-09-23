19401bcf2f107f6244afee55086a2723efcf6904
butter3415 <butter3415@gmail.com>
2023-09-23 18:35:22 +0900

    week04

--- routes/locals.js
+++ routes/locals.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 지역검색 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '홈페이지', pageName:'home.ejs' });
+  res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
 // 렌더링: ejs 파일
 // 루트 - 렌더링(인덱스(ejs파일) 페이지 출력) / 타이틀:홈페이지
 // 인덱스.ejs -> views 폴더
 // 컨트롤러 라우터
 
-
+// 라우터 만들면 app에 등록하기!
 
 module.exports = router;
