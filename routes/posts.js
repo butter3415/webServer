7702c87e49f4328aaa1fb3e4d572e551a1b33ca7
butter3415 <butter3415@gmail.com>
2023-11-18 18:26:50 +0900

    week12

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('index', {title:'게시판', pageName:'posts/list.ejs'})
+    res.render('index', {title:'게시판', pageName:'posts/list.ejs'});
 })
 
 router.get('/insert', function(req, res){
-    res.render('index', {title:'글쓰기', pageName:'posts/insert.ejs'})
+    res.render('index', {title:'글쓰기', pageName:'posts/insert.ejs'});
+})
+
+router.get('/read', function(req, res){
+    const id = req.query.id;
+    res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', id:id});
 })
 
 module.exports = router;
